021b76d2ddebd662bb0d0e2b4465bbc46e77f925
Anuar Kabylbay <kabylbaevanuar@gmail.com>
2020-09-30 00:48:32 +0600

    adding registerViewModel and userManagerResponse data transfer objects (DTO)

---
+++ backend/backend/DataTransferObjects/UserManagerResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace backend.DataTransferObjects
+{
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
