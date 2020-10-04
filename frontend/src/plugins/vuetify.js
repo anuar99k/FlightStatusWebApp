83e5338ad5b9944f6ba36cad0ac4d720bf599710
Anuar Kabylbay <kabylbaevanuar@gmail.com>
2020-10-05 04:47:02 +0600

    adding vuetify.js config file

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,5 +3,19 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+import ru from 'vuetify/es5/locale/ru'
+
+Vue.component('my-component', {
+    methods: {
+		changeLocale () {
+			this.$vuetify.lang.current = 'ru'
+		},
+    },
+})
+
 export default new Vuetify({
+	lang: {
+		locales: { ru },
+		current: 'ru',
+	}
 });
