178413a75762c65828e9b9b85f1a754234a11672
Anuar Kabylbay <kabylbaevanuar@gmail.com>
2020-10-05 05:05:11 +0600

    adding routing configuration (vue-router)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '@/store/index.js'
+// public views
+import Home from '../views/public/Home.vue'
+import Registration from '../views/public/Registration.vue'
+import Login from '../views/public/Login.vue'
+// admin
+import Admin from '@/views/admin/Admin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{ path: '/', name: 'Home', component: Home },
+	{ path: '/registration', name: 'Registration', component: Registration, meta: { requiresVisitor: true } },
+	{ path: '/login', name: 'Login', component: Login },
+	{ path: '/admin', name: 'Admin', component: Admin, meta: { requiresAuth: true } }
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
+
+// routing configuration
+router.beforeEach((to, from, next) => {
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+		// этот путь требует авторизации, проверяем залогинен ли
+		// пользователь, если нет, перенаправляем на страницу логина
+		if (!store.getters.isLoggedIn) {
+			next({name: 'Login'});
+		} else {
+			next();
+		}
+	} else if (to.matched.some(record => record.meta.requiresVisitor)) {
+		if (store.getters.isLoggedIn) {
+			// если пользователь авторизован, то при переходе на страницу
+			// регистрации, он перенаправляется на страницу админа
+			next({name: 'Admin'});
+		} else {
+			next();
+		}
+	} else {
+		next(); // всегда так или иначе нужно вызвать next()
+	}
+});
 
 export default router
